d15c051dc40a850790f74810c543990f6b76ca45
Axelle <axelle.guinaudeau.pro@gmail.com>
2025-04-15 22:37:18 +0200

    :bento: writting page creation

--- src/Pages/Creations.jsx
+++ src/Pages/Creations.jsx
@@ -1,5 +1,91 @@
+import imgHisy from "../assets/img/project/hisy/screenshot_1.jpg";
+
 function Creations() {
-  return <div>Mes créations</div>;
+  return (
+    <>
+      <div className="hero bg-base-200 py-30">
+        <div className="hero-content text-center">
+          <div className="max-w-2xl">
+            <h1 className="text-5xl font-bold">
+              Rien de mieux qu'un tour d'horizon pour vous donner des idées
+            </h1>
+            <p className="py-6">
+              Défilez vers le bas pour voir certains de mes travaux
+            </p>
+            <button className="btn btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-8"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="grid place-items-center py-15">
+        <div className="max-w-2xl px-8">
+          <div className="card lg:card-side bg-base-100 shadow-sm">
+            <div className="card-body">
+              <h2 className="card-title">Hisy</h2>
+              <p>
+                Une application mobile basé sur le challenge #HowISeeYou.
+                Représenter une personne avec neuf photo.
+              </p>
+              <div className="badge badge-primary">React Native</div>
+              <div className="badge badge-neutral">Android</div>
+              <div className="card-actions">
+                <button className="btn btn-ghost">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 192 192"
+                    stroke="currentColor"
+                    class="size-6.5"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="12"
+                      d="M120.755 170c.03-4.669.059-20.874.059-27.29 0-9.272-3.167-15.339-6.719-18.41 22.051-2.464 45.201-10.863 45.201-49.067 0-10.855-3.824-19.735-10.175-26.683 1.017-2.516 4.413-12.63-.987-26.32 0 0-8.296-2.672-27.202 10.204-7.912-2.213-16.371-3.308-24.784-3.352-8.414.044-16.872 1.14-24.785 3.352C52.457 19.558 44.162 22.23 44.162 22.23c-5.4 13.69-2.004 23.804-.987 26.32C36.824 55.498 33 64.378 33 75.233c0 38.204 23.149 46.603 45.2 49.067-3.551 3.071-6.719 9.138-6.719 18.41 0 6.416.03 22.621.059 27.29M27 130c9.939.703 15.67 9.735 15.67 9.735 8.834 15.199 23.178 10.803 28.815 8.265"
+                    />
+                  </svg>
+                </button>
+
+                <button className="btn btn-ghost">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <figure className="max-w-50">
+              <img src={imgHisy} alt="Hisy" />
+            </figure>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Creations;
