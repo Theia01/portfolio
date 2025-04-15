7150f691cd90c914a6ae73d65b01f2af984c9599
Axelle <axelle.guinaudeau.pro@gmail.com>
2025-04-15 17:29:49 +0200

    :lipstick: hide menu hamburger

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,47 +8,41 @@ function NavBar() {
           <a className="btn btn-ghost text-xl logo-app pt-1">Axelle.G</a>
         </div>
         <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
+          <ul className="menu menu-horizontal px-1 max-md:hidden">
             <NavBarLinks />
-            <li>
-              <div className="drawer drawer-end">
-                <input
-                  id="my-drawer-4"
-                  type="checkbox"
-                  className="drawer-toggle"
-                />
-                <div className="drawer-content">
-                  {/* Page content here */}
-                  <label htmlFor="my-drawer-4" className="drawer-button">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      className="inline-block h-5 w-5 stroke-current"
-                    >
-                      {" "}
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h16M4 18h16"
-                      ></path>{" "}
-                    </svg>
-                  </label>
-                </div>
-                <div className="drawer-side">
-                  <label
-                    htmlFor="my-drawer-4"
-                    aria-label="close sidebar"
-                    className="drawer-overlay"
-                  ></label>
-                  <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-                    <NavBarLinks />
-                  </ul>
-                </div>
-              </div>
-            </li>
           </ul>
+          <div className="drawer drawer-end min-md:hidden">
+            <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+              {/* Page content here */}
+              <label htmlFor="my-drawer-4" className="drawer-button">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-5 w-5 stroke-current"
+                >
+                  {" "}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>{" "}
+                </svg>
+              </label>
+            </div>
+            <div className="drawer-side">
+              <label
+                htmlFor="my-drawer-4"
+                aria-label="close sidebar"
+                className="drawer-overlay"
+              ></label>
+              <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                <NavBarLinks />
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
