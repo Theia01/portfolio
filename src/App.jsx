ce31c4f977dac6a914d9118de29a320e92dfc31f
Axelle <axelle.guinaudeau.pro@gmail.com>
2025-04-15 06:34:04 +0200

    :sparkles: add nav

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,38 @@ function App() {
       <header>
         <div className="navbar bg-base-100 shadow-sm">
           <div className="flex-1">
-            <a className="btn btn-ghost text-xl logo-app">Axelle.G</a>
+            <a className="btn btn-ghost text-xl logo-app pt-1">Axelle.G</a>
           </div>
           <div className="flex-none">
-            <button className="btn btn-square btn-ghost">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-5 w-5 stroke-current"
-              >
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-                ></path>{" "}
-              </svg>
-            </button>
+            <ul className="menu menu-horizontal px-1">
+              <li className="p-1">
+                <a>Créations Web & Mobile</a>
+              </li>
+              <li className="p-1">
+                <a>Mon parcours</a>
+              </li>
+              <li className="p-1">
+                <a>Contact</a>
+              </li>
+              <li>
+                <button className="btn btn-square btn-ghost">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="inline-block h-5 w-5 stroke-current"
+                  >
+                    {" "}
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    ></path>{" "}
+                  </svg>
+                </button>
+              </li>
+            </ul>
           </div>
         </div>
       </header>
