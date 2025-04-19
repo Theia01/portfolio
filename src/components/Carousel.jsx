d52e97cde11917293a19078c8405c892c36e3bf1
Axelle <45165141+Theia01@users.noreply.github.com>
2025-04-19 14:47:21 +0000

    :sparkles: add temporary link under the caroussel for large view to scroll

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,19 @@
 function Carousel({ imgs }) {
+  const hash = (Math.random() + 1).toString(36).substring(5)
+
   const itemsCarousel = imgs.map((img, i) => (
-    <div key={i} className="carousel-item md:w-full">
+    <div key={i} id={`${hash}-${i}`} className="carousel-item md:w-full">
       <img src={img} className="md:w-full max-md:max-h-120" alt="" />
     </div>
   ));
+  const linksImg = imgs.map((img, i) => (
+    <a href={`#${hash}-${i}`} className="btn btn-xs">{i + 1}</a>
+  ));
 
-  return <div className="carousel rounded-box md:w-64">{itemsCarousel}</div>;
+  return (<>
+    <div className="carousel rounded-box md:w-64">{itemsCarousel}</div>
+    <div className="max-md:hidden flex justify-center space-x-1 mt-1">{linksImg}</div>
+  </>)
 }
 
 export default Carousel;
